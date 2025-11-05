ac216434655eda2e0b7ffb675daeefbf7e1ff5cb
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-05 22:42:36 +0200

    Delete alx-react-app-new/src/Component directory

--- alx-react-app-new/src/Component/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
-      <p>© 2025 My Favorite Cities. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
