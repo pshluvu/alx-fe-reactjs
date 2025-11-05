d39415f1d955c142014207f168ac46d488e8debf
pshluvu <percivalshimange@gmail.com>
2025-11-06 00:27:21 +0200

    Add my-company folder with all files as normal folder

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f1f1f1' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
