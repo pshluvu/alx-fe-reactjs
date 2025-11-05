17edcaca5a29ec2f75b4f6db518e690c81deaccd
pshluvu <percivalshimange@gmail.com>
2025-11-05 22:00:36 +0200

    Enhanced components with inline CSS styling

---
+++ alx-react-app-new/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2025 My Favorite Cities. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
