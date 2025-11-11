48173dce7834bf3a942b7281ea92dd565744c749
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-11 22:28:27 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,23 +1,27 @@
 
 import create from 'zustand';
 
-export const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(r => r.id === updatedRecipe.id ? updatedRecipe : r)
+  })),
+  deleteRecipe: (id) => set(state => ({
+    recipes: state.recipes.filter(r => r.id !== id)
+  })),
   
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  
-  setRecipes: (recipes) => set({ recipes }),
-
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  // Search & Filtering
+  searchTerm: '',
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
+    get().filterRecipes(); // trigger filtering whenever term changes
+  },
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    )
+  })),
 }));
+
