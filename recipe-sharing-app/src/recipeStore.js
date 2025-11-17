b0677acc534cc98c1f0c9583d28ca52125a8f0ce
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-11-17 21:21:58 +0200

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -3,8 +3,7 @@ import { create } from 'zustand';
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
 }));
+
