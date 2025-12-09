c7d48f7c2bf4d37efd3d8d988dd31fcbb9a78577
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-09 21:35:28 +0200

    Add ProtectedRoute for profile route

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
-import BlogPost from "./components/BlogPost"; // ✅ Import BlogPost
+import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -9,10 +10,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 
-        {/* Profile route */}
-        <Route path="profile/*" element={<Profile />} />
+        {/* Protected Profile Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
 
-        {/* ✅ Dynamic route */}
+        {/* Dynamic Blog Post Route */}
         <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
