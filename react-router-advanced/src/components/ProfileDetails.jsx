4824ed4f555a451bf6b50183d92c86d6dd35848f
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-09 21:26:41 +0200

    Add ProfileDetails component for user profile display

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,10 @@
+function ProfileDetails() {
+  return (
+    <div>
+      <h3>Profile Details Page</h3>
+      <p>Here you can see your profile information.</p>
+    </div>
+  );
+}
+
+export default ProfileDetails;
