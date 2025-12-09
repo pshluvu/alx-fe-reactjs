9871988a5187aefc28425ee575ac29038fc0fae4
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-09 21:31:55 +0200

    Refactor routing in App component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
-import Home from "./components/Home"; // optional home page
+import BlogPost from "./components/BlogPost"; // ✅ Import BlogPost
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        
-        {/* ✅ Protected Profile Route (simplified) */}
-        <Route path="profile" element={<Profile />}>
-          {/* Nested Routes */}
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+
+        {/* Profile route */}
+        <Route path="profile/*" element={<Profile />} />
+
+        {/* ✅ Dynamic route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
