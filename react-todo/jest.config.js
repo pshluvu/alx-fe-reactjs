3acc7a730fd16e679386989e654618b69f4c3e9a
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-09 22:05:44 +0200

    Delete react-todo directory

--- react-todo/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
-  moduleFileExtensions: ["js", "jsx"],
-};
