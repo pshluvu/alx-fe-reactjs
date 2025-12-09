8d257c255e73aaa679cbfb8bbaf83d0d2d8b79c6
pshluvu <93921716+pshluvu@users.noreply.github.com>
2025-12-09 21:27:16 +0200

    Add ProfileSettings component for user profile updates

---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,10 @@
+function ProfileSettings() {
+  return (
+    <div>
+      <h3>Profile Settings Page</h3>
+      <p>Here you can update your profile settings.</p>
+    </div>
+  );
+}
+
+export default ProfileSettings;
